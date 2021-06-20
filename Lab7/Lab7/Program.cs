5fbdc990546eee9edb0891a5cdb6457ea5d1ca5c
Morgan Bateman <mbateman855@gmail.com>
2021-06-20 12:47:34 -0400

    added some regex funcionality

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NameValidation("Morgan Bateman"));
+            Console.WriteLine(DateValidation("07/26/1996"));
+           
+        }
+        static bool NameValidation(string name)
+        {
+            Regex nameValidation = new Regex(@"[A-Z][a-z]{1,30}");
+            return nameValidation.IsMatch(name);
+        }
+        static bool DateValidation(string date)
+        {
+            Regex dateValidation = new Regex(@"\d{2}/\d{2}/\d{4}");
+            return dateValidation.IsMatch(date);
+        }
+    }
+}
